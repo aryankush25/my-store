dd4de1f4af4d08c4bc250ddc7dc8b001a0db2c18
Aryan Agarwal <aryankush025@gmail.com>
2020-06-12 00:12:00 +0530

    auth guard setup

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,21 +1,38 @@
 <script>
-  import Nav from "../components/Nav.svelte";
+  import * as R from "ramda";
+  import { unProtectedRoutes } from "../utils/helper.js";
+  import { isTokensPresentLocalStorage } from "../services/utils/helper.js";
+  import { goto } from "@sapper/app";
 
   export let segment;
+
+  $: path = segment ? `/${segment}` : "/";
+
+  const fetchAuthGuard = async route => {
+    try {
+      const isLoggedIn = isTokensPresentLocalStorage();
+      const isProtectedRouted = !R.includes(route, unProtectedRoutes);
+
+      if (isLoggedIn && !isProtectedRouted) {
+        if (window) await goto("/");
+      } else if (!isLoggedIn && isProtectedRouted) {
+        if (window) await goto("/login");
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  $: fetchAuthGuard(path);
+
+  $: showHeader = !R.includes(path, unProtectedRoutes);
 </script>
 
 <style type="text/scss">
   main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+    font-family: monospace;
   }
 </style>
-
-<Nav {segment} />
 
 <main>
   <slot />
