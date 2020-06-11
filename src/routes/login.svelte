5dbd788d24e6600a8a6dcafb42ecfca39f0a79a8
Aryan Agarwal <aryankush025@gmail.com>
2020-06-11 23:50:20 +0530

    login register components added

---
+++ src/routes/login.svelte
@@ -0,0 +1,94 @@
+<script>
+  import { goto } from "@sapper/app";
+
+  import LoginSignUpPageContainer from "../containers/LoginSignUpPageContainer/index.svelte";
+  import SharedInput from "../components/shared/SharedInput/index.svelte";
+  import SharedCheckbox from "../components/shared/SharedCheckbox/index.svelte";
+  import SharedButton from "../components/shared/SharedButton/index.svelte";
+  import api from "../services";
+
+  import { emailValidator, passwordValidator } from "../utils/validators.js";
+
+  let rememberMe = false;
+  let email = "";
+  let password = "";
+  let isLoading = false;
+
+  $: validatorEmail = emailValidator(email);
+  $: validatorPassword = passwordValidator(password);
+
+  const onChangeEmail = event => {
+    const { value } = event;
+
+    email = value;
+  };
+
+  const onChangePassword = event => {
+    const { value } = event;
+    password = value;
+  };
+
+  const handleOnSubmit = async () => {
+    isLoading = true;
+    const response = await api.loginUserApi(email, password);
+    isLoading = false;
+
+    if (response.success) {
+      goto("/");
+    }
+  };
+
+  $: isValidFormData = validatorEmail.isValid && validatorPassword.isValid;
+</script>
+
+<style>
+  .form-heading {
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 40px;
+  }
+</style>
+
+<svelte:head>
+  <title>Task Management App - Login</title>
+</svelte:head>
+
+<LoginSignUpPageContainer reverse={false}>
+  <img slot="image" src={'/images/signin-image.jpg'} alt="login-img" />
+
+  <a slot="change-link" href="/register">Create an account</a>
+
+  <form slot="main-form" on:submit|preventDefault>
+    <div class="form-heading">Sign in</div>
+
+    <SharedInput
+      type="text"
+      name="email"
+      label="Email"
+      value={email}
+      onChange={onChangeEmail}
+      placeholder="Enter Email"
+      error={validatorEmail.errorMessage} />
+
+    <SharedInput
+      type="password"
+      name="password"
+      label="Password"
+      value={password}
+      onChange={onChangePassword}
+      placeholder="Enter Password"
+      error={validatorPassword.errorMessage} />
+
+    <SharedCheckbox
+      name="rememberMe"
+      bind:checked={rememberMe}
+      label="Remember me" />
+
+    <SharedButton
+      label="Log in"
+      name="login-button"
+      isDisabled={!isValidFormData}
+      {isLoading}
+      on:click={handleOnSubmit} />
+  </form>
+</LoginSignUpPageContainer>
---
+++ src/routes/register.svelte
@@ -0,0 +1,138 @@
+<script>
+  import { goto } from "@sapper/app";
+
+  import LoginSignUpPageContainer from "../containers/LoginSignUpPageContainer/index.svelte";
+  import SharedInput from "../components/shared/SharedInput/index.svelte";
+  import SharedCheckbox from "../components/shared/SharedCheckbox/index.svelte";
+  import SharedButton from "../components/shared/SharedButton/index.svelte";
+  import api from "../services";
+
+  import {
+    nameValidator,
+    emailValidator,
+    passwordValidator,
+    confirmPasswordValidator
+  } from "../utils/validators.js";
+
+  let name = "";
+  let email = "";
+  let password = "";
+  let confirmPassword = "";
+  let isLoading = false;
+
+  $: validatorName = nameValidator(name);
+  $: validatorEmail = emailValidator(email);
+  $: validatorPassword = passwordValidator(password);
+  $: validatorConfirmPassword = confirmPasswordValidator(
+    password,
+    confirmPassword
+  );
+
+  const onChangeName = event => {
+    const { value } = event;
+    name = value;
+  };
+
+  const onChangeEmail = event => {
+    const { value } = event;
+    email = value;
+  };
+
+  const onChangePassword = event => {
+    const { value } = event;
+    password = value;
+  };
+
+  const onChangeConfirmPassword = event => {
+    const { value } = event;
+    confirmPassword = value;
+  };
+
+  const handleOnSubmit = async () => {
+    isLoading = true;
+    const response = await api.registerUserApi(name, email, password);
+
+    isLoading = false;
+
+    if (response.success) {
+      goto("/");
+    }
+  };
+
+  $: isValidFormData =
+    validatorName.isValid &&
+    validatorEmail.isValid &&
+    validatorPassword.isValid &&
+    validatorConfirmPassword.isValid;
+</script>
+
+<style>
+  .form-heading {
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 40px;
+  }
+
+  .register-button {
+    padding-top: 20px;
+  }
+</style>
+
+<svelte:head>
+  <title>Task Management App - Register</title>
+</svelte:head>
+
+<LoginSignUpPageContainer reverse={true}>
+  <img slot="image" src={'/images/signup-image.jpg'} alt="register-img" />
+
+  <a slot="change-link" href="/login">I am already member</a>
+
+  <form slot="main-form" on:submit|preventDefault>
+    <div class="form-heading">Sign up</div>
+
+    <SharedInput
+      type="text"
+      name="name"
+      label="Name"
+      value={name}
+      onChange={onChangeName}
+      placeholder="Your Name"
+      error={validatorName.errorMessage} />
+
+    <SharedInput
+      type="text"
+      name="email"
+      label="Email"
+      value={email}
+      onChange={onChangeEmail}
+      placeholder="Enter Email"
+      error={validatorEmail.errorMessage} />
+
+    <SharedInput
+      type="password"
+      name="password"
+      label="Password"
+      value={password}
+      onChange={onChangePassword}
+      placeholder="Enter Password"
+      error={validatorPassword.errorMessage} />
+
+    <SharedInput
+      type="password"
+      name="confirmPassword"
+      label="Confirm Password"
+      value={confirmPassword}
+      onChange={onChangeConfirmPassword}
+      placeholder="Confirm Password"
+      error={validatorConfirmPassword.errorMessage} />
+
+    <div class="register-button">
+      <SharedButton
+        label="Register"
+        name="login-button"
+        isDisabled={!isValidFormData}
+        {isLoading}
+        on:click={handleOnSubmit} />
+    </div>
+  </form>
+</LoginSignUpPageContainer>
