a98edb168772acee9ffba8a6f85efd9aa73fd70c
Aryan Agarwal <aryankush025@gmail.com>
2020-06-19 02:02:42 +0530

    home container added

---
+++ src/containers/HomePageContainer/index.svelte
@@ -0,0 +1,35 @@
+<style type="text/scss">
+  .home-page-container {
+    background-color: #bde7f0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .home-page-wrapper {
+    background-color: #ffffff;
+    height: 90%;
+    width: 90%;
+    border-radius: 30px;
+    box-sizing: border-box;
+    padding: 30px 0;
+  }
+
+  .home-page-inner-wrapper {
+    height: 100%;
+    width: 100%;
+    overflow: auto;
+    padding: 0 30px;
+    box-sizing: border-box;
+  }
+</style>
+
+<div class="home-page-container">
+  <div class="home-page-wrapper">
+    <div class="home-page-inner-wrapper">
+      <slot />
+    </div>
+  </div>
+</div>
